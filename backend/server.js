273d3f19f55899aeb430243f94a380e9a7305ed8
altusmarketingdigital-mx <altusmarketingdigital@gmail.com>
2026-07-12 23:18:15 -0600

    Fix express json payload limit

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const app = express();
 
 // Middlewares
 app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000', credentials: true }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
 // Archivos estáticos (Frontend)
